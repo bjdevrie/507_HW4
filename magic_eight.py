c09ea6869fd6ed363e185b7b7de73e6cf7e54208
BDevries <bjdevrie@umich.edu>
2018-02-02 12:07:20 -0500

    initial branch commit

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,13 @@
-usr_input = input("What is your question? \n")
+def add_questions():
+    pass
 
+run = True
+while run == True:
+    usr_input = str(input("\nWhat is your question?\n"))
+    if usr_input.lower() == 'quit':
+        run = False
+        break
+    if usr_input.endswith('?') == True:
+        pass  #random print statement from "add questions" go here
+    else:
+        print ("I'm sorry, I can only answer questions.")
