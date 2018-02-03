e50c42c8a3efd37b9b84c124d039ed9df8c1b987
B.DeVries <bjdevrie@umich.edu>
2018-02-03 01:11:03 -0500

    v.2

--- magic_eight.py
+++ magic_eight.py
@@ -1,11 +1,10 @@
 def add_questions():
     pass
 
-run = True
-while run == True:
+
+while True:
     usr_input = str(input("\nWhat is your question?\n"))
     if usr_input.lower() == 'quit':
-        run = False
         break
     if usr_input.endswith('?') == True:
         pass  #random print statement from "add questions" go here
